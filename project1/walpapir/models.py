67cb88766fef9d427e666db309c39c74df8de18f
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2018-12-29 21:47:48 +0900

    on the read of setting point column

--- project1/walpapir/models.py
+++ project1/walpapir/models.py
@@ -52,6 +52,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=150, blank=True)
 
+    #point has user point.
+    point = models.IntegerField()
+
     is_staff = models.BooleanField(
         _('staff status'),
         default=False,
