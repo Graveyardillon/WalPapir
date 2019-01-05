158cf6c7faf6d2ce46931aa5992862509713722f
makikyou <44311434+makikyou@users.noreply.github.com>
2019-01-06 00:28:49 +0900

    m

--- project1/walpapir/models.py
+++ project1/walpapir/models.py
@@ -101,6 +101,9 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         return self.email
 
-
-
-
+from datetime import datetime
+class Photo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='walpapir_image')#ファイルの名前
+    title = models.CharField(max_length=30)
+    time = models.DateTimeField(default=datetime.now)#自分で時間を設定する場合はdefaultはいらない。
