35632b2db76aec32b654fd3d215ea90294113614
maru713 <maru713114@gmail.com>
2019-01-11 11:43:51 +0900

    placeholder and sign up button fixed

--- project1/walpapir/forms.py
+++ project1/walpapir/forms.py
@@ -29,3 +29,4 @@ class UserCreateForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
